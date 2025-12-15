7a61c465ed87a2cecb97f84aa31608762ac16053
Jonnathan Perez <perezjota360@gmail.com>
2025-12-15 08:16:29 -0500

    Update modelo_botella_hija1.py

--- botella/botellas/modelo_botella_hija1.py
+++ botella/botellas/modelo_botella_hija1.py
@@ -6,22 +6,25 @@ class Botella_plastico(Botella):
         
         
         
-    def asignacion_material(self, dato_material, dato_capacidad, dato_forma, dato_diseño, dato_tapa, dato_grabados):  
+    def asignacion_material(self, dato_materiales, dato_capacidad, dato_forma, dato_diseño, dato_tapa, dato_grabados):  
         super().asignacion_material(dato_material, dato_capacidad, dato_forma, dato_diseño, dato_tapa, dato_grabados)     
         self.material = dato_material
         self.capacidad = dato_capacidad
         self.forma = dato_forma
         self.diseño = dato_diseño
-        self.tapa = dato_tapa
         self.grabados = dato_grabados
+        self.tapa = dato_tapa
         
-    def asignacion_material2(self, dato_material, dato_capacidad, dato_forma, dato_diseño, dato_tapa, dato_grabados) :
+        
+    def asignacion_material2(self, dato_materiales, dato_capacidad, dato_forma, dato_tapa, dato_grabados, dato_diseño) :
         self.material = dato_material
         self.capacidad = dato_capacidad
         self.forma = dato_forma
         self.diseño = dato_diseño
-        self.tapa = dato_tapa
         self.grabados = dato_grabados   
+        self.tapa = dato_tapa
+       
     
     def compatible_bebidas_calientes(self):
-        return "No es compatible para bebidas muy calientes."
+        return "No es compatible con bebidas calientes."
+
