7367a51146f7ce9849f5d279f110e7fd01bf69ae
Jonnathan Perez <perezjota360@gmail.com>
2025-12-15 08:27:22 -0500

    Update modelo_vehiculo_MINI_VAN.py
    Updated the MiniVan class to specify light cargo capacity and few passengers.

--- vehiculos/vehiculos/modelo_vehiculo_MINI_VAN.py
+++ vehiculos/vehiculos/modelo_vehiculo_MINI_VAN.py
@@ -1,11 +1,12 @@
 from modelo_vehiculo import Vehiculo
 
 class MiniVan(Vehiculo):
-    def __init__(self, modelo, color, motor, num_puertas, capacidad_pasajeros, tipo_combustible):
-        super().__init__(modelo, color, motor, num_puertas, capacidad_pasajeros, tipo_combustible)
+    def __init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible):
+        super().__init__(modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible)
 
     def capacidad_carga(self):
-        return f"El {self.modelo} está diseñada para transportar carga y pasajeros."
+        return f"El {self.modelo} está diseñada para transportar carga liviana y pasajeros pocos pasajeros."
 
     def sistema_ventanas(self):
         return f"El {self.modelo} tiene ventanas manuales o eléctricas según versión."
+
