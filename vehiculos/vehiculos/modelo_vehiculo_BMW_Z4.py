7b2edf916aca6effb972dbd8d68ddb898c0522d1
Jonnathan Perez <perezjota360@gmail.com>
2025-12-15 08:22:07 -0500

    Update modelo_vehiculo_BMW_Z4.py

--- vehiculos/vehiculos/modelo_vehiculo_BMW_Z4.py
+++ vehiculos/vehiculos/modelo_vehiculo_BMW_Z4.py
@@ -1,11 +1,12 @@
 from modelo_vehiculo import Vehiculo
 
 class BMW_Z4(Vehiculo):
-    def __init__(self, modelo, color, motor, num_puertas, capacidad_pasajeros, tipo_combustible):
-        super().__init__(modelo, color, motor, num_puertas, capacidad_pasajeros, tipo_combustible)
+    def __init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible):
+        super().__init__(modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible)
 
     def aceleracion_frenado(self):
         return f"El {self.modelo} tiene aceleración deportiva y frenado de alta precisión."
 
     def tipo_seguridad(self):
         return f"El {self.modelo} cuenta con airbags, frenos ABS y control de estabilidad."
+
