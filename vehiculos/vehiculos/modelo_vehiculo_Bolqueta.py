844e395d2d1f86f2b25e11d1d9a5551ce50b3fca
Jonnathan Perez <perezjota360@gmail.com>
2025-12-15 08:24:29 -0500

    Update modelo_vehiculo_Bolqueta.py

--- vehiculos/vehiculos/modelo_vehiculo_Bolqueta.py
+++ vehiculos/vehiculos/modelo_vehiculo_Bolqueta.py
@@ -1,11 +1,12 @@
 from modelo_vehiculo import Vehiculo
 
 class Bolqueta(Vehiculo):
-    def __init__(self, modelo, color, motor, num_puertas, capacidad_pasajeros, tipo_combustible):
-        super().__init__(modelo, color, motor, num_puertas, capacidad_pasajeros, tipo_combustible)
+    def __init__(self, modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible):
+        super().__init__(modelo, color, motor, numero_puertas, capacidad_pasajeros, tipo_combustible)
 
     def transporte_carga(self):
         return f"La {self.modelo} se utiliza principalmente para transportar materiales de construcción o carga pesada."
 
     def sistema_direccion(self):
         return f"La {self.modelo} tiene dirección reforzada para soportar cargas pesadas."
+
